2d192f33046890f017b45490960e5d88ed71933b
chungngocnguyen <chungptit2002@gmail.com>
2023-05-21 01:27:11 +0700

    test_ci/cd

---
+++ test_api.py
@@ -0,0 +1,56 @@
+import unittest
+import requests
+
+
+class TestApp(unittest.TestCase):
+
+
+    def test_insert_data_api(self):
+        data = {
+            "id": "100",
+            "name": "chungngoc",
+            "username": "chungnn",
+            "birth": "2002",
+            "sex": "Male",
+            "university": "PTIT",
+            "major": "dtvt"
+        }
+        response = requests.post("http://localhost/api/create/", json=data)
+
+        responseJson = response.json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(responseJson["id"], "100")
+
+        requests.get("http://localhost/api/delete/100")
+
+    def test_update_data_api(self):
+        data = {
+            "id": "100",
+            "name": "chungngoc",
+            "username": "chungnn",
+            "birth": "2002",
+            "sex": "Male",
+            "university": "PTIT",
+            "major": "dtvt"
+        }
+        response = requests.post("http://localhost/api/update/", json=data)
+
+        responseJson = response.json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(responseJson["id"], "100")
+
+    def test_read_data_api(self):
+        response = requests.get("http://localhost/api/read/15")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()[0]['id'], '15')
+
+    def test_delete_data_api(self):
+        response = requests.get("http://localhost/api/delete/0")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.text, "Delete successfully")
+
+if __name__ == '__main__':
+    unittest.main()
